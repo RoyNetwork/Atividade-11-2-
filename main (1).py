952f07b60dab83d3f193912df0cab31773fb8bcf
RoyNetwork <106408391+RoyNetwork@users.noreply.github.com>
2022-06-03 15:00:44 -0300

    Atividade 11 (salário)
    Projeto Sasageyo

---
+++ main (1).py
@@ -0,0 +1,6 @@
+__Author__ = 'Alan (Roy)'
+
+salario = float(input('Digite o salário atual: ').replace(',','.'))
+novo_salario = salario*0.35+salario
+print("Novo salário = R%.2f"%novo_salario)
+print("Seu sálario aumentou 0.36%")
